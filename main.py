1becb18cf28ab91f6a3595993568cf16c4c86042
Kamran Raei <kamrad1372@gmail.com>
2025-05-18 09:28:48 +0330

    ++++

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ def hello_world():
 
 if __name__ == '__main__':
     print("++++++++++++++++++++++++++++++++++")
+    print("++++++++++++++++++++++++++++++++++")
     app.run(debug=False, host="0.0.0.0", port=5000)
